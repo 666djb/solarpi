432d10ccb1ec7e7dc38442e80946a3f1e2ff3ffc
David Brown <david@djb.org.uk>
2022-12-20 17:05:49 +0000

    Update

--- src/solarpi.ts
+++ src/solarpi.ts
@@ -2,24 +2,30 @@ import GrowattClient from 'growatt-modbus'
 
 console.log("Starting SolarPi")
 
-try{
-runSolarPi()
-} catch (error){
-console.log("Error")
+try {
+    runSolarPi()
+} catch (error) {
+    if (error instanceof Error) {
+        console.error(`Error reading data: ${error.message}`)
+    } else {
+        console.error("Unknown error:", error)
+    }
 }
 
+console.log("Ending")
+
 async function runSolarPi() {
-    //const GrowattClient = await import("growatt-modbus")
     const growattClient = new GrowattClient({
         baudRate: 9600,
         device: '/dev/ttyAMA0',
         modbusId: 1
     })
+
     try {
         await growattClient.init()
         const data = await growattClient.getData()
+        console.log(`Data: ${data}`)
     } catch (error) {
-        console.error("Error reading data:", error.message)
-	throw(error)
+        throw (error)
     }
 }
