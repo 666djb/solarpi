2a50a7a1d341ce2948bc95aa08d6387695f87d23
David Brown <david@djb.org.uk>
2022-12-27 18:28:34 +0000

    testing

--- dist/solarpi.js
+++ dist/solarpi.js
@@ -3,7 +3,7 @@ import { getConfig } from "./config.js";
 import { getEntities, getEntityfromMap } from "./growattEntities.js";
 import { GrowattClient } from "./growattClient.js";
 import { logDate } from "./logDate.js";
-console.log(`${Date().toLocaleString()} Starting SolarPi`);
+console.log(`${logDate()} Starting SolarPi`);
 const CONFIG_FILE = "options.json";
 const config = getConfig(CONFIG_FILE);
 const publisher = new Publisher(config.mqtt, getEntities());
