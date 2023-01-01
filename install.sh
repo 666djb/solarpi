816fcaac9728b6968320e70bb0957e77ce05151b
David Brown <david@djb.org.uk>
2023-01-01 14:38:24 +0000

    Updated installer

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
+pwd=$(pwd)
 echo "Adding solarpi system user"
-sudo adduser --system --no-create-home --group solarpi
+sudo adduser --system --group solarpi
 echo "Creating /opt/solarpi directory"
 sudo mkdir -p /opt/solarpi
 echo "Copying files"
 sudo cp -r . /opt/solarpi/
-sudo chown -r solarpi:solarpi /opt/solarpi
+sudo chown -R solarpi:solarpi /opt/solarpi
 echo "Getting Node.JS modules and compiling solarpi"
 cd /opt/solarpi
-sudo su solarpi npm install
+sudo -u solarpi npm install
 echo "Creating systemd service for solarpi"
-cp solarpi.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable solarpi
+sudo cp solarpi.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable solarpi
 echo "Starting solarpi service"
-#service myapp start
+#sudo service myapp start
+cd $pwd
